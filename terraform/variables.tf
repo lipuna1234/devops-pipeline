d302174dd49203d9dfc168ddf725a5230a925718
lipuna1234 <chandrakantasahoo021@gmail.com>
2026-08-26 20:10:17 +0530

    Make Terraform Kubernetes resources runtime configurable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,50 @@
+variable "kubeconfig_path" {
+  description = "Path to kubeconfig used by Terraform"
+  type        = string
+  default     = "~/.kube/config"
+}
+
+variable "kube_context" {
+  description = "Kubernetes context used by Terraform"
+  type        = string
+  default     = "kind-devops"
+}
+
+variable "namespace" {
+  description = "Existing Kubernetes namespace in which the application is deployed"
+  type        = string
+}
+
+variable "app_name" {
+  description = "Application name"
+  type        = string
+}
+
 variable "app_image" {
   description = "Container image to deploy"
   type        = string
-  default     = "devops-app:local"
+}
+
+variable "replicas" {
+  description = "Number of application replicas"
+  type        = number
+  default     = 2
+}
+
+variable "container_port" {
+  description = "Application container port"
+  type        = number
+  default     = 5000
+}
+
+variable "service_port" {
+  description = "Kubernetes Service port"
+  type        = number
+  default     = 80
+}
+
+variable "config" {
+  description = "Environment-specific ConfigMap values"
+  type        = map(string)
+  default     = {}
 }
